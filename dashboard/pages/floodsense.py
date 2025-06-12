5f4a630508813c62c0bde2de1f46702831a42f10
ajibulriski <ajibul.riski@gmail.com>
2025-06-12 13:02:52 +0700

    memperbarui branch local

--- dashboard/pages/floodsense.py
+++ dashboard/pages/floodsense.py
@@ -1,6 +1,7 @@
 import reflex as rx
 from ..templates import template
 from ..backend import FloodPredictionModel
+import os
 
 from ..views.map_display import south_sulawesi_map_display
 model = FloodPredictionModel.get_instance()
@@ -105,7 +106,7 @@ def prediction_input_form() -> rx.Component:
     title="FloodSense",
     description="FloodSense is a web application that provides real-time flood monitoring and alerts.",
     route="/floodsense",
-    on_load=model.load_if_needed("../dashboard/dashboard/models/lstm_smote_cv.h5") 
+    on_load=model.load_if_needed(f"{os.getcwd()}/dashboard/models/lstm_smote_cv.h5") 
 )
 def floodsense():
     return rx.hstack(
